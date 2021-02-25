69cf33eb47c3d5df83dadb2558597ec70d0cf304
TBK <tbk@jjtc.eu>
2021-02-25 13:52:22 +0100

    Entrypoint tweaks

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 
-if [ ! -f /app/.env ]; then
+if [ ! -f .env ]; then
     php artisan key:generate --no-interaction --force
 fi
 php artisan migrate --no-interaction --force
@@ -9,19 +9,5 @@ php artisan migrate --no-interaction --force
 php artisan cache:clear
 php artisan view:clear
 
-echo "Getting PPM ready:"
-trapIt() {
-    "$@" &
-    pid="$!"
-    for SGNL in INT TERM CHLD USR1; do
-        trap "kill -$SGNL $pid" "$SGNL";
-    done
-    while kill -0 $pid >/dev/null 2>&1; do
-        wait $pid
-        ec="$?"
-    done
-    exit $ec
-}
-
-echo "Starting Nginx & PPM:"
-multirun "nginx" "$( trapIt /ppm/vendor/bin/ppm start --ansi --no-interaction --config=ppm.json )"
+echo "Starting PPM:"
+/ppm/vendor/bin/ppm start --ansi --no-interaction --config=ppm.json
